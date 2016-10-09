7cc7e8f8c2a077fa01d691c64d99b6a2cb581d9e
rafeeqpk36 <rafeeqpk36@gmail.com>
2016-10-09 23:29:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,67 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={title:"article-one/Rafeeq",
+                heading:"Atricle One",
+                date:"9th Oct 2016",
+                content:`<p>
+             This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+         </p>
+         <p>
+             This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+         </p>
+         <p>
+             This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first art
+         icle.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+         </p>`};
+         
+         var articleTwo={title:"article-Two/Rabah",
+                heading:"Atricle Two",
+                date:"10th Oct 2016",
+                content:`<p>
+             This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article
+         </p>`};
+         
+        var articleThree={title:"article-Three/Rashid",
+                heading:"Atricle Three",
+                date:"11th Oct 2016",
+                content:`<p>This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article </p>`}; 
+                function createTemplate(data){var title=data.title;
+                                              var heading=data.heading;
+                                              var date=data.date;
+                                              var content=data.content;
+                                              var htmlTemplate:`<!doctype html>
+<html>
+ <head>
+     <title>${title}</title>
+     <meta name="viewport" content="width=device-width,initial-scale=1"/>
+     <link href="ui/style.css" type="text/css" rel="stylesheet"/>
+ </head>
+ <body>
+     <div class="container">
+     <div>
+         <a href='/'>Home</a>
+     </div>
+     <hr/>
+     <div>
+         <h1>${heading}</h1>
+     </div>
+     <div>
+        ${date}
+     </div>
+     <div>
+         ${content}
+         </div>
+         </div>
+ </body>
+</html>`;
+return htmlTemplate
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));});
-app.get('/article-two',function(req,res){res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));});
-app.get('/article-three',function(req,res){res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));});
-app.get('/article-four',function(req,res){res.send('it is the article about FATHIMA RIDHA')});
+app.get('/:articleName',function(req,res){res.send(createTemplate(articleName));});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
