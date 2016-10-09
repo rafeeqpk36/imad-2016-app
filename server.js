260bde9d7a162b90c4df28db643046cb1445772f
rafeeqpk36 <rafeeqpk36@gmail.com>
2016-10-09 07:58:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.get('/article-four',function(req,res){res.send('it is the article about FATH
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/rafi.jpeg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'rafi.jpeg'));
 });
 
 
