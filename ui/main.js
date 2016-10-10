66de70991138888db4b5dca59931054f16eec8cd
rafeeqpk36 <rafeeqpk36@gmail.com>
2016-10-10 18:32:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,5 @@
 console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="new value";
+var element=document.getElementById("image");
+img.onclick=function(){img.style.moveLeft="100px";};
