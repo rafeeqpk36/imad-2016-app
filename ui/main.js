09abfd062ee609e6c9f3b237be5d1130601fe997
rafeeqpk36 <rafeeqpk36@gmail.com>
2016-10-10 23:54:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var button=document.getElementById("counter");
 button.onclick=function(){
     var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){if(request.readystate===XMLHttpRequest.DONE){ if(request.status===200)
+    request.onreadystatechange=function(){if(request.readyState===XMLHttpRequest.DONE){ if(request.status===200)
     {var counter=request.responseText;
     var span=document.getElementById("count");
     span.innerHTML=counter.toString();}}};
