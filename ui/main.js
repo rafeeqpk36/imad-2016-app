ea84ac46f0d7b62cbf2f672c0dcaff9a96d8bc22
rafeeqpk36 <rafeeqpk36@gmail.com>
2016-10-10 18:50:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,9 @@ console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="new value";
 var img=document.getElementById("image");
-img.onclick=function(){img.style.marginLeft="100px";};
+var marginLeft=0;
+function moveRight(){marginLeft=marginLeft+10;
+                     img.style.marginLeft=marginLeft+'px'}
+img.onclick=function(){var interval=setinterval(moveRight,100)};
+    
+    
