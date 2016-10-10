cd1b371bc333163c4acf475fec05828a581fe21b
rafeeqpk36 <rafeeqpk36@gmail.com>
2016-10-10 10:37:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,7 +69,8 @@ app.get('/ui/rafi.jpeg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'rafi.jpeg'));
 });
 
-
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`RAFEEQ IMAD course  app listening on port ${port}!`);
