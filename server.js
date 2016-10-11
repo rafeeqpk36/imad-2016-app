240b9edfba9ce53bfcf5a0cc6beb9edd7ac12e15
rafeeqpk36 <rafeeqpk36@gmail.com>
2016-10-11 08:58:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,6 +64,8 @@ app.get('/counter', function (req, res) {counter=counter+1;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
   });
+  var names=[];
+  app.get('/submit-name',function(req,res){var name=req.query.name;names.push(name);res.send(JSON.stringify(names));});
   app.get('/:articleName',function(req,res){var articleName=req.params.articleName;res.send(createTemplate(articles[articleName]));});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
