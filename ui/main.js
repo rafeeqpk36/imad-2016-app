1a38defc1825ac1cd6f77b3590f05e4f4632efd3
rafeeqpk36 <rafeeqpk36@gmail.com>
2016-10-11 07:42:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,4 +8,17 @@ button.onclick=function(){
     request.open('GET','http://rafeeqpk36.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var submit=document.getElementById("submit_btn");
+submit.onclick=functon(){
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+='<li>'names[i]'<li>'
+    };};
+    
+
+
+   
     
