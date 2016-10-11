7af42617b84f249513e75c4f0dbd0f2feefcb6cd
rafeeqpk36 <rafeeqpk36@gmail.com>
2016-10-11 07:57:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,10 +15,10 @@ submit.onclick=functon(){
     var names=['name1','name2','name3'];
     var list='';
     for(var i=0;i<names.length;i++){
-        list+='<li>'names[i]'<li>'
-    };};
-    var ul=document.getElementById("name");
-    ul.innerHTML=list;
+        list+='<li>'+names[i]+'<li>'
+    }
+    var ul=document.getElementById("namelist");
+    ul.innerHTML=list;}
     
 
 
