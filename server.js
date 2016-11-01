4ecb548dc4b74f54c430685dae65518a1e788e8f
rafeeqpk36 <rafeeqpk36@gmail.com>
2016-11-01 18:05:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').Pool;
+var config={user:'rafeeqpk36',
+            database:'rafeeqpk36',
+            host:'db.imad.hasura-app.io',
+            port:'5432',
+            password:process.env.DB_PASSWORD};
+var pool=new Pool(config);    
+            
 var app = express();
 app.use(morgan('combined'));
 
@@ -64,7 +71,7 @@ app.get('/counter', function (req, res) {counter=counter+1;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
   });
-  app.get('/test-db',function(req,res){  });
+  app.get('/test-db',function(req,res){pool.query('SELECT *FROM test',function(err,result){if(err){res.status(500).send(err.toString());}else{res.send(JSON.stringify(result))}})});
   var names=[];
   app.get('/submit-name',function(req,res){var name=req.query.name;names.push(name);res.send(JSON.stringify(names));});
   app.get('/:articleName',function(req,res){var articleName=req.params.articleName;res.send(createTemplate(articles[articleName]));});
