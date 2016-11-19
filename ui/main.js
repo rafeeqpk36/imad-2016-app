b446d0c713137e1313c267021205d4a04f4a1049
rafeeqpk36 <rafeeqpk36@gmail.com>
2016-11-19 07:31:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,29 @@
+// submit through login/password
+var submit= document.getElementById("submit_btn");
+//creat request
+submit.onclick=function()
+   { var request=new XMLHttpRequest();
+   request.onreadystatechange=function(){
+       if(request.readystate===XMLHttpRequest.DONE){
+           if(request.status===200){
+               alert("Logged in successfully");}
+               else if(request.status===403){
+                   alert("Incorrect Username/Password");}
+                   else if(request.status===500){alert("Server not responding");
+               }
+           }
+       };
+
+//request part
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+console.log(username);
+console.log(password);
+request.open('POST','http://http://rafeeqpk36.imad.hasura-app.io/login',true);
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({username:username, password:password}));
+};
+
 var button=document.getElementById("counter");
 button.onclick=function(){
     var request=new XMLHttpRequest();
