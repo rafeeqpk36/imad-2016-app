3dc22d17159da36bb7e4a18becd555bd4bb94033
rafeeqpk36 <rafeeqpk36@gmail.com>
2016-11-23 06:06:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,6 +55,13 @@ app.get('/counter', function (req, res) {counter=counter+1;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
   });
+  function hash(input,salt){
+      var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'Sha512');
+      return hashed.toString('hex');
+  }
+  app.get('/hash/:input',function(req,res){var hashedString=hash(req.params.input,"some random string");
+                                               res.send(hasedString);
+  });
   app.get('/test-db',function(req,res){pool.query('SELECT *FROM test',function(err,result){if(err){res.status(500).send(err.toString());}else{res.send(JSON.stringify(result.rows))}})});
   var names=[];
   app.get('/submit-name',function(req,res){var name=req.query.name;names.push(name);res.send(JSON.stringify(names));});
