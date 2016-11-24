4769d0b26f8fcb8dcbb37af0c76f816a96803b60
rafeeqpk36 <rafeeqpk36@gmail.com>
2016-11-24 06:44:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,40 +19,8 @@ var username=document.getElementById("username").value;
 var password=document.getElementById("password").value;
 console.log(username);
 console.log(password);
-request.open('POST','http://http://rafeeqpk36.imad.hasura-app.io/login',true);
+request.open('POST','http://rafeeqpk36.imad.hasura-app.io/login',true);
 request.setRequestHeader('Content-Type','application/json');
 request.send(JSON.stringify({username:username, password:password}));
 };
 
-var button=document.getElementById("counter");
-button.onclick=function(){
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){if(request.readyState===XMLHttpRequest.DONE){ if(request.status===200)
-    {var counter=request.responseText;
-    var span=document.getElementById("count");
-    span.innerHTML=counter.toString();}}};
-    request.open('GET','http://rafeeqpk36.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
-var submit=document.getElementById("submit_btn");
-submit.onclick = function(){ var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){if(request.readyState===XMLHttpRequest.DONE){ if(request.status===200)
-   { names=request.responseText;
-   names=JSON.parse(names);
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+ names[i] +'</li>';
-    }
-    var ul=document.getElementById("namelist");
-    ul.innerHTML=list;
-    
-}}};
-var nameInput=document.getElementById("name");
-var name=nameInput.value;
-  request.open('GET','http://rafeeqpk36.imad.hasura-app.io/submit-name?name='+name,true);
-  request.send(null);
-};
-
-
-   
-    
