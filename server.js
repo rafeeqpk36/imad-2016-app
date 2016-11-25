b0ec1e6e0f4cc083815005d9a9617ae4767068de
rafeeqpk36 <rafeeqpk36@gmail.com>
2016-11-25 23:43:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.use(morgan('combined'));
 app.use(bodyparser.json());
 app.use(session({secret:'someRandomSecretValue',
                  cookie:{maxAge:10000*60*60*24*30},
-                 resave: true,
-                 saveUninitialized: true
+                 
 }));
                 function createTemplate (data) {
     var title = data.title;
