6aa55781f8c773398bd514531317cf0073903bcc
rafeeqpk36 <rafeeqpk36@gmail.com>
2016-11-26 07:18:14 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -83,7 +83,7 @@ var currentArticleTitle = window.location.pathname.split('/')[2];
 	                }
 	                comments.innerHTML = content;
 	            } else {
-	                alert('Oops! Could not load comments!');
+	                comments.innerHTML('Oops! Could not load comments!');
 	            }
 	        }
 	    };
